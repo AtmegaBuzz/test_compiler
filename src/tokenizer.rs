c8f5cffd1639f0ba40007708cb70ec2c6e063c89
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-04-18 01:24:12 +0530

    added more grammer rules

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,26 +3,31 @@ use std::fmt;
 use regex::Regex;
 
 enum TokenType{
-    NUMBER,
+    INT,
+    FLOAT,
+    CHAR,
     STRING,
     OPERATOR,
-    SPECIAL,
-    VARIABLE
+    ASSIGNMENT,
+    INDENTIFIER,
 }
-
 impl fmt::Display for TokenType {
 
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
         match self{
-            TokenType::NUMBER => write!(f, "NUMBER()"),
-            TokenType::STRING => write!(f, "STRING()"),
-            TokenType::OPERATOR => write!(f, "OPERATOR()"),
-            TokenType::SPECIAL => write!(f, "SPECIAL()"),
-            TokenType::VARIABLE => write!(f, "VARIABLE()")
+            TokenType::INT => write!(f, "INT"),
+            TokenType::FLOAT => write!(f, "FLOAT"),
+            TokenType::CHAR => write!(f, "CHAR"),
+            TokenType::STRING => write!(f, "STRING"),
+            TokenType::OPERATOR => write!(f, "OPERATOR"),
+            TokenType::ASSIGNMENT => write!(f, "ASSIGNMENT"),
+            TokenType::INDENTIFIER => write!(f, "INDENTIFIER")
         }
     }
 
 }
+
+
 
 pub struct Token{
     token_type: TokenType,
@@ -55,12 +60,39 @@ impl Lexer{
 
         
         let num_re = Regex::new(r"[0-9]").unwrap();
-        let variable_reg = Regex::new(r"^[a-zA-Z]*[a-zA-Z_]").unwrap();
-
-        while self.counter != self.content_len{
+        let variable_reg = Regex::new(r"[a-zA-Z]*[a-zA-Z_]").unwrap();
+        let type_reg = Regex::new(r"[a-z]").unwrap();
+        
+        while self.counter < self.content_len{
             
             let chr = self.content.chars().nth(self.counter as usize).unwrap();
-
+            
+            if type_reg.is_match(&chr.to_string()) && self.content.len()-7 > (self.counter as usize){
+                
+                let counter = self.counter as usize; 
+                if &self.content[counter..counter+3] == "int" {
+                    self.tokens.push(Token{token_type: TokenType::INT, value: String::from("int")});
+                    self.counter += 4;
+                    continue;
+                }
+                else if &self.content[counter..counter+5] == "float" {
+                    self.tokens.push(Token{token_type: TokenType::FLOAT, value: String::from("float")});
+                    self.counter += 6;
+                    continue;
+                }
+                else if &self.content[counter..counter+6] == "string" {
+                    self.tokens.push(Token{token_type: TokenType::STRING, value: String::from("string")});
+                    self.counter += 7;
+                    continue;
+                }
+                else if &self.content[counter..counter+4] == "char" {
+                    self.tokens.push(Token{token_type: TokenType::CHAR, value: String::from("char")});
+                    self.counter += 5;
+                    continue;
+                }
+                
+            }
+            
             if chr == '\n' || chr == ' '{
                 self.counter += 1;
                 continue;
@@ -68,11 +100,16 @@ impl Lexer{
 
             else if "+-*/".contains(chr){
                 self.tokens.push(Token{token_type: TokenType::OPERATOR, value: chr.to_string()});
+                self.counter += 1;
+                continue;
             }
             
             else if "(){},;=:".contains(chr){
-                self.tokens.push(Token{token_type: TokenType::SPECIAL, value: chr.to_string()});
+                self.tokens.push(Token{token_type: TokenType::ASSIGNMENT, value: chr.to_string()});
+                self.counter += 1;
+                continue;
             }
+
 
             else if chr == '\'' || chr == '"'{
                 self.tokens.push(Token{token_type: TokenType::STRING, value: chr.to_string()});
@@ -87,10 +124,13 @@ impl Lexer{
                         break;
                     }
 
+                    if chr == '\'' && string.len() > 1{
+                        panic!("char expected passed string");
+                    }
+
                     string.push(string_char);
                     self.counter += 1;
                 }
-
                 self.tokens.push(Token{token_type: TokenType::STRING, value: string});
                 self.tokens.push(Token{token_type: TokenType::STRING, value: chr.to_string()});
                 self.counter += 1;
@@ -110,7 +150,8 @@ impl Lexer{
                     self.counter += 1;
                 
                 }
-                self.tokens.push(Token { token_type: TokenType::VARIABLE, value: variable });
+                self.tokens.push(Token { token_type: TokenType::INDENTIFIER, value: variable});
+                continue;
             }
 
             else if num_re.is_match(&chr.to_string()){
@@ -127,10 +168,12 @@ impl Lexer{
                     nummber.push(num_chr.clone());
                     self.counter += 1;
                 }
-                self.tokens.push(Token { token_type: TokenType::NUMBER, value: nummber });
+                self.tokens.push(Token { token_type: TokenType::INT, value: nummber});
+                continue;
             
             }
-            
+
+
             else {
                 panic!("unexpected token recieved");
             }
